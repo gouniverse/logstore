85b0d2dcdc5c58c4279c67b4a1f6936381efa85d
Milan Lesichkov <lesichkovm@gmail.com>
2022-06-26 11:09:47 +0000

    Updated dependencies

--- go.mod
+++ go.mod
@@ -6,5 +6,4 @@ require (
 	github.com/doug-martin/goqu/v9 v9.18.0
 	github.com/gouniverse/uid v1.3.0
 	github.com/mattn/go-sqlite3 v1.14.9
-	gorm.io/gorm v1.22.4
 )
--- log.go
+++ log.go
@@ -2,9 +2,6 @@ package logstore
 
 import (
 	"time"
-
-	"github.com/gouniverse/uid"
-	"gorm.io/gorm"
 )
 
 const (
@@ -26,16 +23,16 @@ const (
 
 // Log type
 type Log struct {
-	ID      string     `gorm:"type:varchar(40);column:id;primary_key;"`
-	Level   string     `gorm:"type:varchar(10);column:level;"`
-	Message string     `gorm:"type:varchar(510);column:message;"`
-	Context string     `gorm:"type:longtext;column:context;"`
-	Time    *time.Time `gorm:"type:datetime;column:time;DEFAULT NULL;"`
+	ID      string
+	Level   string
+	Message string
+	Context string
+	Time    *time.Time
 }
 
 // BeforeCreate adds UID to model
-func (l *Log) BeforeCreate(tx *gorm.DB) (err error) {
-	uuid := uid.HumanUid()
-	l.ID = uuid
-	return nil
-}
+// func (l *Log) BeforeCreate(tx *gorm.DB) (err error) {
+// 	uuid := uid.HumanUid()
+// 	l.ID = uuid
+// 	return nil
+// }
